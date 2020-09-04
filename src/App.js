c2f592029ab6f09a8923817431ca947599aceeb0
OnismoMudavanhu <onismomudavanhu@gmail.com>
2020-09-04 19:57:33 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 
  export default function App() {
@@ -9,6 +10,7 @@ import NavBar from './components/NavBar';
        <Header/>
        <NavBar/>       
       <p className="text-center text-red-600 font-semibold">Onismo Mudavanhu demo app running (react and tailwind css) .....</p> 
+      <Footer/>
      </div>
        
    );
